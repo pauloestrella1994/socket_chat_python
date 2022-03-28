9d115f3e2040d7475613a953d22e5642c046b783
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-03-27 23:36:36 -0300

    Refactore client

--- client.py
+++ client.py
@@ -1,8 +1,23 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
-  client_message = input()
-  client.sendto(client_message.encode(), (socket.gethostbyname(socket.gethostname()), 12000))
-  message_bytes, ip_address_server = client.recvfrom(2048)
+def client(client_connection):
+  while True:
+    client_message = input()
+    client_connection.sendto(client_message.encode(), (socket.gethostbyname(socket.gethostname()), 12000))
+    message_bytes, ip_address_server = client_connection.recvfrom(2048)
+    print(f"Receiving message from server: {message_bytes.decode()}")
+    if message_bytes.decode() == "q":
+      print(f"Closing client connection with the server ip: {ip_address_server[0]}; port: {ip_address_server[1]}")
+      client_connection.close()
+      break
+
+if __name__ == "__main__":
+  try:
+    client_connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print(f"Start connection with the server: {client_connection}")
+    client(client_connection)
+  except socket.error as e:
+    print(f"A socket connecting problem occur: {e}")
+  finally:
+    client_connection.close()
