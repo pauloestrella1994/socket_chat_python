a1770c7b47e84fcd88316c264db2598709078d6d
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-03-27 23:36:47 -0300

    Refactore server

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('', 12000))
 
-while True:
-  message_bytes, ip_address_server = server.recvfrom(2048)
-  message = message_bytes.decode()
-  server.sendto(message.encode(), ip_address_server)
-  print(message)
+def server(server_connection):
+  print(f"Server up and listening port: 12000")
+  while True:
+    message_bytes, ip_address_server = server_connection.recvfrom(2048)
+    message = message_bytes.decode()
+    print(f"Message from client: {message}")
+    server_connection.sendto(message.encode(), ip_address_server)
+  
+if __name__ == "__main__":
+  try:
+    server_connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_connection.bind((socket.gethostbyname(socket.gethostname()), 12000))
+    server(server_connection)
+  except socket.error as e:
+    print(f"A socket connecting problem occur: {e}")
+  except KeyboardInterrupt as e:
+    print("Closing server connection")
+  finally:
+    server_connection.close()
