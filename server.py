2897f9502d816e4ec6f4c85fe3deb14c1219a38e
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-03-28 13:49:42 -0300

    Use try except to handle problems in socket

--- server.py
+++ server.py
@@ -3,10 +3,14 @@ import socket
 
 def server(server_connection):
   print(f"Server up and listening port: 12000")
-  while True:
-    message_bytes, ip_address_server = server_connection.recvfrom(2048)
-    print(f"Message from client: {message_bytes.decode()}")
-    server_connection.sendto(message_bytes, ip_address_server)
+  try:
+    while True:
+      message_bytes, ip_address_server = server_connection.recvfrom(2048)
+      print(f"Message from client: {message_bytes.decode()}")
+      server_connection.sendto(message_bytes, ip_address_server)
+  except socket.error as e:
+    print(f"A error occur in socket: {e}")
+    server_connection.close()
   
 if __name__ == "__main__":
   try:
