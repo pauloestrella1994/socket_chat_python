a130b1ed679a4f798282f730b3cce18dbe1455a5
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-03-30 01:56:35 -0300

    Refactoring server to a class

--- server.py
+++ server.py
@@ -1,52 +1,55 @@
 import threading, socket
 
 
-host = '127.0.0.1'
-port = 55555
+class Server:
+  def __init__(self) -> None:
+      self.clients = []
+      self.nicknames = []
+      return
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
+  def broadcast(self, message: str) -> None:
+    for client in self.clients:
+      client.send(message)
+    return
 
-clients = []
-nicknames = []
+  def handle(self, client: socket.socket):
+    while True:
+      try:
+        message = client.recv(1024)
+        self.broadcast(message)
+      except:
+        index = self.clients.index(client)
+        self.clients.remove(client)
+        client.close()
+        nickname = self.nicknames[index]
+        self.broadcast(f'{nickname} left chat!'.encode())
+        self.nicknames.remove(nickname)
+        break
 
-def broadcast(message):
-  for client in clients:
-    client.send(message)
+  def receive(self):
+    while True:
+      client, address = server.accept()
+      print(f"Connected with {str(address)}")
 
-def handle(client):
-  while True:
-    try:
-      message = client.recv(1024)
-      broadcast(message)
-    except:
-      index = clients.index(client)
-      clients.remove(client)
-      client.close()
-      nickname = nicknames[index]
-      broadcast(f'{nickname} left chat!'.encode())
-      nicknames.remove(nickname)
-      break
+      client.send('NICK'.encode())
+      nickname = client.recv(1024).decode()
+      self.nicknames.append(nickname)
+      self.clients.append(client)
 
-def receive():
-  while True:
-    client, address = server.accept()
-    print(f"Connected with {str(address)}")
+      print(f'Nickname ok the client is {nickname}!')
+      self.broadcast(f"{nickname} joined the chat!".encode())
+      client.send('Connected to the server!'.encode())
 
-    client.send('NICK'.encode())
-    nickname = client.recv(1024).decode()
-    nicknames.append(nickname)
-    clients.append(client)
+      thread = threading.Thread(target=self.handle, args=(client,))
+      thread.start()
 
-    print(f'Nickname ok the client is {nickname}!')
-    broadcast(f"{nickname} joined the chat!".encode())
-    client.send('Connected to the server!'.encode())
-
-    thread = threading.Thread(target=handle, args=(client,))
-    thread.start()
 
 if __name__ == '__main__':
+  host = '127.0.0.1'
+  port = 55555
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server.bind((host, port))
+  server.listen()
   print("Welcome to the server chat room!")
   print("Waiting for clients...")
-  receive()
+  Server().receive()
