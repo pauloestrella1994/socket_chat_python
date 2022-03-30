c0ddd584fc398595e3b4c55f93c635ff94bfeada
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-03-30 02:40:13 -0300

    Refatoring client using class

--- client.py
+++ client.py
@@ -1,33 +1,42 @@
 import threading, socket
 
 
-host = '127.0.0.1'
-port = 55555
-nickname = input("choose a nickname: ")
+class Client:
+  def receive(self, client: socket.socket):
+    while True:
+      try:
+        message = client.recv(1024).decode()
+        if message == 'NICK':
+          client.send(nickname.encode())
+        else:
+          print(message)
+      except:
+        print('An error occurred!')
+        client.close()
+        break
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+  def write(self, client: socket.socket):
+    while True:
+      try:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode())
+      except:
+        if client:
+          client.close()
 
-def receive():
-  while True:
-    try:
-      message = client.recv(1024).decode()
-      if message == 'NICK':
-        client.send(nickname.encode())
-      else:
-        print(message)
-    except:
-      print('An error occurred!')
-      client.close()
-      break
+if __name__ == '__main__':
+  host = '127.0.0.1'
+  port = 55555
+  nickname = input("choose a nickname: ")
+  try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    client_class = Client()
+    receive_thread = threading.Thread(target=client_class.receive, args=(client,))
+    receive_thread.start()
 
-def write():
-  while True:
-    message = f'{nickname}: {input("")}'
-    client.send(message.encode())
-
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
+    write_thread = threading.Thread(target=client_class.write, args=(client,))
+    write_thread.start()
+  except KeyboardInterrupt:
+    print("Closing client!")
+    client.close()
